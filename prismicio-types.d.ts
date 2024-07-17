57e60e7345a28fb4076f84f404c0c8640e8fddc6
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2024-07-17 10:56:46 +0200

    removing mills slice

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -73,7 +73,6 @@ export type HeaderDocument<Lang extends string = string> =
   >;
 
 type HomepageDocumentDataSlicesSlice =
-  | MillsSliceSlice
   | CtaSliceSlice
   | FaqSliceSlice
   | TeamSectionSlice;
@@ -330,51 +329,6 @@ export type CtaSliceSlice = prismic.SharedSlice<
 >;
 
 /**
- * Primary content in *MillsSlice → Primary*
- */
-export interface MillsSliceSliceDefaultPrimary {
-  /**
-   * Title field in *MillsSlice → Primary*
-   *
-   * - **Field Type**: Rich Text
-   * - **Placeholder**: *None*
-   * - **API ID Path**: mills_slice.primary.title
-   * - **Documentation**: https://prismic.io/docs/field#rich-text-title
-   */
-  title: prismic.RichTextField;
-}
-
-/**
- * Default variation for MillsSlice Slice
- *
- * - **API ID**: `default`
- * - **Description**: Default
- * - **Documentation**: https://prismic.io/docs/slice
- */
-export type MillsSliceSliceDefault = prismic.SharedSliceVariation<
-  "default",
-  Simplify<MillsSliceSliceDefaultPrimary>,
-  never
->;
-
-/**
- * Slice variation for *MillsSlice*
- */
-type MillsSliceSliceVariation = MillsSliceSliceDefault;
-
-/**
- * MillsSlice Shared Slice
- *
- * - **API ID**: `mills_slice`
- * - **Description**: MillsSlice
- * - **Documentation**: https://prismic.io/docs/slice
- */
-export type MillsSliceSlice = prismic.SharedSlice<
-  "mills_slice",
-  MillsSliceSliceVariation
->;
-
-/**
  * Primary content in *TeamSection → Primary*
  */
 export interface TeamSectionSliceDefaultPrimary {
@@ -483,10 +437,6 @@ declare module "@prismicio/client" {
       CtaSliceSliceDefaultPrimary,
       CtaSliceSliceVariation,
       CtaSliceSliceDefault,
-      MillsSliceSlice,
-      MillsSliceSliceDefaultPrimary,
-      MillsSliceSliceVariation,
-      MillsSliceSliceDefault,
       TeamSectionSlice,
       TeamSectionSliceDefaultPrimary,
       TeamSectionSliceDefaultItem,
